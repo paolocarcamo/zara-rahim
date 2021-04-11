d2725a1664f3f91f4108411edc5ffeb8927f980a
Paolo Carcamo <Pogi@Paolo-Carcamo.local>
2021-04-10 20:52:13 -0700

    Initial Commit: Homepage @ 80% / Other areas under construction

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,52 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: '',
+  headline: 'Is your current project lacking clarity?',
+  description: '',
+  buttonLabel: '',
+  imgStart: '',
+  img: 'images/confusion.svg',
+  alt: 'Credit Card',
+};
+
+export const homeObjTwo = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: '',
+  headline: 'Is your business stuck and looking for solutions?',
+  description: '',
+  buttonLabel: '',
+  imgStart: 'start',
+  img: 'images/solution.svg',
+  alt: 'Vault',
+};
+
+export const homeObjThree = {
+  lightBg: false,
+  lightText: false,
+  lightTextDesc: true,
+  topLine: '',
+  headline:
+    'Or are you having a hard time navigating the complex media, cultural, and political landscape',
+  description: '',
+  buttonLabel: '',
+  imgStart: '',
+  img: 'images/compass.svg',
+  alt: 'Vault',
+};
+
+export const homeObjFour = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: '',
+  headline: 'Let me be your wayfinder.',
+  description: '',
+  buttonLabel: '',
+  imgStart: 'start',
+  img: 'images/direction.svg',
+  alt: 'Vault',
+};
---
+++ src/components/pages/Services/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import { homeObjFour } from './Data';
+
+function Services() {
+  return (
+    <>
+      <HeroSection {...homeObjFour} />
+    </>
+  );
+}
+
+export default Services;
