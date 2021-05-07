9301ec4a2b27e76951068a1220eac5a50c194e90
Paolo Carcamo <Pogi@Paolo-Carcamo.local>
2021-05-07 09:36:08 -0700

    Built initial iteration of contact form submission functionality

--- src/components/pages/GetInTouch/GetInTouch.js
+++ src/components/pages/GetInTouch/GetInTouch.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import './GetInTouch.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function GetInTouch() {
+  const [status, setStatus] = useState('Submit');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus('Sending...');
+    const { name, email, subject, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      subject: subject.value,
+      message: message.value,
+    };
+    let response = await fetch('http://localhost:5000/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus('Submit');
+    let result = await response.json();
+    alert(result.status);
+  };
   return (
     <>
       <div
@@ -11,34 +34,34 @@ function GetInTouch() {
         data-aos-offset='10'
         data-aos-duration='1000'
       >
-        <form action=''>
+        <form onSubmit={handleSubmit}>
+          <Link to='/' className='home-link'>
+            <h1>ZARA RAHIM</h1>
+          </Link>
           <div className='form-inputs'>
-            <Link to='/' className='home-link'>
-              <h1>ZARA RAHIM</h1>
-            </Link>
             <label htmlFor='name' className='form-label' id='name-label'>
               name
             </label>
-            <input type='text' name='name' className='form-input' />
+            <input type='text' id='name' className='form-input' required />
           </div>
           <div className='form-inputs'>
             <label htmlFor='email' className='form-label' id='email-label'>
               email
             </label>
-            <input type='email' name='email' className='form-input' />
+            <input type='email' id='email' className='form-input' required />
           </div>
           <div className='form-inputs'>
             <label htmlFor='subject' className='form-label' id='subject-label'>
               subject
             </label>
-            <input type='text' name='subject' className='form-input' />
+            <input type='text' id='subject' className='form-input' required />
           </div>
           <div className='form-inputs' id='text-box'>
             <label htmlFor='message' className='form-label'>
               message
             </label>
             <textarea
-              name='name'
+              id='message'
               rows='8'
               columns='50'
               className='form-input'
